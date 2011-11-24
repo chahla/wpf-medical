a5cad30875877cf8f3dc793c51e938e59d81fc4d
Pascal Arnould <pascal1.arnould@gmail.com>
2011-11-24 13:53:27 +0100

    Affichage de graphe pas encore terminé code pas totalement stable

--- Wcf-Medical/Wpf-Medical/ServiceLiveCallback.cs
+++ Wcf-Medical/Wpf-Medical/ServiceLiveCallback.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
 
 namespace Wpf_Medical
 {
-    class ServiceLiveCallback : Wpf_Medical.ServiceLive.IServiceLiveCallback
+    public class ServiceLiveCallback : Wpf_Medical.ServiceLive.IServiceLiveCallback
     {
+        private static ObservableCollection<KeyValuePair<string, double>> _heartlist;
+        private static ObservableCollection<KeyValuePair<string, double>> _templist;
+
+        public static ObservableCollection<KeyValuePair<string, double>> Heartlist
+        {
+            get { return ServiceLiveCallback._heartlist; }
+            set { ServiceLiveCallback._heartlist = value; }
+        }
+
+        public static ObservableCollection<KeyValuePair<string, double>> Templist
+        {
+            get { return ServiceLiveCallback._templist; }
+            set { ServiceLiveCallback._templist = value; }
+        }
+
         public ServiceLiveCallback()
         {
+            _heartlist = new ObservableCollection<KeyValuePair<string, double>>();
+            _templist = new ObservableCollection<KeyValuePair<string, double>>();
         }
 
         public void PushDataHeart(double requestData)
         {
             Debug.WriteLine(requestData);
+            string time = DateTime.Now.TimeOfDay.Hours + ":" + DateTime.Now.TimeOfDay.Minutes + ":" + DateTime.Now.TimeOfDay.Seconds;
+            _heartlist.Add(new KeyValuePair<string, double>(time, requestData));
         }
 
         public void PushDataTemp(double requestData)
         {
             Debug.WriteLine(requestData);
+            string time = DateTime.Now.TimeOfDay.Hours + ":" + DateTime.Now.TimeOfDay.Minutes + ":" + DateTime.Now.TimeOfDay.Seconds;
+            _templist.Add(new KeyValuePair<string, double>(time, requestData));
         }
 
     }
